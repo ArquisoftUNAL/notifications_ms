85da2dd0d8a5aea7f0a1fb32d7104fb49c949a05
dnrubiol <dnrubiol@unal.edu.co>
2023-09-30 17:09:07 -0500

    functional cronjob sidekiq

--- app/workers/check_notifications_worker.rb
+++ app/workers/check_notifications_worker.rb
@@ -5,9 +5,22 @@ class CheckNotificationsWorker
       # Perform a simple check to see if the notifications are working
       puts "Checking notifications..."
       
+      # Print relevant notifications
+      current_time = Time.now
+
+      puts "Time: #{current_time}" 
+
       # Print all notifications
       Notification.all.each do |notification|
-        puts "Notification: #{notification.not_id} - #{notification.not_description}"
+        #puts "Notification: #{notification.noti_title} - #{notification.noti_init_date}"
+        
+        if notification.noti_init_date <= current_time && notification.noti_active == true
+          puts "Notification: #{notification.noti_title} - #{notification.noti_init_date}"
+          notification.update(noti_active: false)
+        end
       end
-    end               
+    end   
+    
   end
+
+
