2dbbd8d4ed5443c5af5de0731238c36d6e8f0cbd
edgardanielgd <edgardanielgd123@gmail.com>
2023-10-02 22:00:57 -0500

    Added production host to config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << ENV["HOST"]
+
 rescue StandardError => e
   puts "Error al conectar a RabbitMQ: #{e.message}"
 
